90ad0d20f4aa3b728e8a0c936891a1970f946a81
Chetan Patil <49969478+spyhydra@users.noreply.github.com>
2023-02-28 11:26:13 +0530

    movie-card and reducer

---
+++ src/components/MovieCard.js
@@ -0,0 +1,32 @@
+import React,{Component} from 'react';
+
+
+
+class MovieCard extends React.Component{
+
+render (){
+    const  {movie } = this.props;
+    console.log(movie);
+  return (
+    <div className="movie-card">
+      <div className='left'>
+          <img alt='movie-poster' src={movie.Poster}/>
+      </div>
+      <div className='right'>
+          <div className='title'>{movie.Title}</div>
+          <div className='Plot'>{movie.Plot}</div>
+          <div className='footer'>
+              <div className='rating'>{movie.idmbRating}</div>
+              <button className='favourite-btn'>favourite</button>
+          </div>
+      </div>
+
+    </div>
+  );
+
+}
+
+  
+}
+
+export default MovieCard;
